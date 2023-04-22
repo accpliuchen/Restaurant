9c5ac80bbfa5180bfaf004e145eeedc7e97a53c3
cl6016 <cl6016@nyu.edu>
2023-04-22 00:13:49 -0400

    Initial commit

---
+++ src/main/java/com/example/SpringRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+public class SpringRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestaurantApplication.class, args);
+    }
+}
