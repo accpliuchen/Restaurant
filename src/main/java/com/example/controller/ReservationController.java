864ff92639be3027857aaff3502cc5868813599a
cl6016 <cl6016@nyu.edu>
2023-04-23 23:58:33 -0400

    update

--- src/main/java/com/example/controller/ReservationController.java
+++ src/main/java/com/example/controller/ReservationController.java
@@ -1,10 +1,12 @@
 package com.example.controller;
 
+import com.example.domain.Table;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 @RestController
@@ -22,5 +25,35 @@ public class ReservationController {
 
     private static final Logger logger = LoggerFactory.getLogger(ReservationController.class);
 
+    private List<Table> tableList = new ArrayList<Table>();
 
+    public ReservationController(){
+        Table table=new Table();
+        table.setTableId(1);
+        table.setNumbers(2);
+        tableList.add(table);
+    }
+
+    @RequestMapping(value = "/getAvailableSlots", method = { RequestMethod.GET, RequestMethod.POST })
+    public ResponseEntity<String> getAvailableSlots() {
+        return new ResponseEntity(tableList, HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/getBookedTimeSlots", method = { RequestMethod.GET, RequestMethod.POST })
+    public ResponseEntity<String> getBookedTimeSlots() {
+        return ResponseEntity.ok("Hello World!");
+    }
+
+
+    @RequestMapping(value = "/bookRequest", method = { RequestMethod.GET, RequestMethod.POST })
+    public ResponseEntity<String> bookRequest() {
+        return ResponseEntity.ok("Hello World!");
+    }
+
+
+//    @RequestMapping(value = "/getAvailableSlots", method = { RequestMethod.GET, RequestMethod.POST })
+//    public ResponseEntity<String> getAvailableSlots(@RequestParam("durationInMinutes") int durationInMinutes) {
+//        return ResponseEntity.ok("Hello World!");
+//    }
 }
---
+++ src/main/java/com/example/domain/Table.java
@@ -0,0 +1,90 @@
+package com.example.domain;
+
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.date.DateUtil;
+
+import java.io.*;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+import java.util.Locale;
+import java.util.Set;
+import java.util.HashSet;
+
+public class Table implements Serializable {
+	private int tableId;
+	private int numbers;
+
+	public int getTableId() {
+		return tableId;
+	}
+
+	public void setTableId(int tableId) {
+		this.tableId = tableId;
+	}
+
+	public int getNumbers() {
+		return numbers;
+	}
+
+	public void setNumbers(int numbers) {
+		this.numbers = numbers;
+	}
+
+	//private HashSet<Slot> timeslot;
+
+
+	public static void main(String args[]) throws ParseException {
+//		String startTime="9:30";
+//		String endTime="10:30pm";
+//
+////		String beginTime = "2018-07-28 14:42:32";
+////
+////		String endTime = "2018-07-29 12:26:32";
+//
+////		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+//
+//		SimpleDateFormat sdf = new SimpleDateFormat("hh:mm a");
+//
+////		SimpleDateFormat sdformat = new SimpleDateFormat("yyyy-MM-dd");
+////		Date startDate=sdf.parse(sdformat.format(new Date())+" " +startTime);
+////		Date endDate=sdf.parse(sdformat.format(new Date())+" "+ endTime);
+//
+//
+//		//System.out.println(DateUtil.between(startDate,endDate, DateUnit.HOUR));
+//
+//		Date now = new Date();
+//
+//		Date afterDate = new Date(now.getTime() + 300000);
+//		System.out.println(sdf.format(afterDate ));
+//
+////	    if(startDate.before(endDate) && endDate.after(startDate)){
+////	    	System.out.println("Over");
+////	    }
+
+
+		Date date = DateUtil.date();
+		SimpleDateFormat sdf1 = new SimpleDateFormat("dd-MM-yyyy");// 设置日期格式
+		Date now = new Date();
+		String today = sdf1.format(now);
+		String startTime = "9:30am";
+		String endTime = "1:30am";
+		String beginDate = today + " " + startTime;
+		String endDate = today + " " + endTime;
+		String strDateFormat = "dd-MM-yyyy hh:mmaa";
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		try {
+			Date date1 = new SimpleDateFormat(strDateFormat, Locale.ENGLISH).parse(beginDate);
+			Date date2 = new SimpleDateFormat(strDateFormat, Locale.ENGLISH).parse(endDate);
+			long between = DateUtil.between(date1, date2, DateUnit.HOUR);
+			System.out.println(between + "");
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		System.out.println("===");
+
+	}
+}
